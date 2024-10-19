bf2bf2a568393a5754c47ea459c15586de1086c5
CodeGallantX <johnayobami77@proton.me>
2024-10-19 19:42:06 +0100

    ..

--- classA/settings.py
+++ classA/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 # import dj_database_url
 
@@ -135,3 +136,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
